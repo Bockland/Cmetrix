64aab8f23d268600e12df02fbbdf78415d44313e
Bockland <Bockland@users.noreply.github.com>
2018-09-25 11:04:02 -0300

    QueryExpression
    Create QueryExpression funtions

---
+++ QueryExpression.cs
@@ -0,0 +1,19 @@
+public static void Link(IOrganizationService Service)
+{
+    string EntityPrincipal = "";
+    string EntitySecundario = "";
+    string Relationship = "";
+    QueryExpression Consulta = new QueryExpression();
+    Consulta.EntityName = EntityPrincipal;
+    Consulta.ColumnSet = new ColumnSet(true);
+    Consulta.Criteria.AddCondition(new ConditionExpression("statuscode", ConditionOperator.Equal, 0));
+    LinkEntity L1 = new LinkEntity(Relationship, EntitySecundario, "atributolink1", "atributolink1", JoinOperator.Inner);
+    L1.Columns = new ColumnSet(true);
+    L1.LinkCriteria.AddCondition(new ConditionExpression("statuscode", ConditionOperator.Equal, 0));
+    LinkEntity L2 = new LinkEntity("Relacion", "Entity3", "atributolink", "atributolink", JoinOperator.Inner);
+    L2.Columns = new ColumnSet(true);
+    L2.LinkCriteria.AddCondition(new ConditionExpression("statuscode", ConditionOperator.Equal, 0));
+    L1.LinkEntities.Add(L2);
+    Consulta.LinkEntities.Add(L1);
+    EntityCollection Resultado = Service.RetrieveMultiple(Consulta);
+}
