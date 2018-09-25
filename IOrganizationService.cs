61f7073c4e826f5ad7d570fd8a34f7c25bd27a1e
Bockland <Bockland@users.noreply.github.com>
2018-09-25 16:21:45 -0300

    IOrganizationService
    Class connection to CRM from C#

---
+++ IOrganizationService.cs
@@ -0,0 +1,24 @@
+public static void ServicioCRM_2011()
+{
+    string CredencialUsuario = System.Configuration.ConfigurationManager.AppSettings["CredencialUsuario"];
+    string CredencialContrasena = System.Configuration.ConfigurationManager.AppSettings["CredencialContrasena"];
+    string CredencialDominio = System.Configuration.ConfigurationManager.AppSettings["CredencialDominio"];
+    string CRMOrganizationName = System.Configuration.ConfigurationManager.AppSettings["CRMOrganizationName"];
+    string CRMServidor = System.Configuration.ConfigurationManager.AppSettings["CRMServidor"];
+
+
+    System.Net.NetworkCredential credenciales = new System.Net.NetworkCredential(CredencialUsuario, CredencialContrasena, CredencialDominio);
+    ClientCredentials Credentials = new ClientCredentials();
+    Credentials.Windows.ClientCredential = credenciales;
+    Uri OrganizationUri = new Uri("http://" + CRMServidor + "/" + CRMOrganizationName + "/XRMServices/2011/Organization.svc");
+
+
+    Uri HomeRealmUri;
+    HomeRealmUri = null;
+
+    OrganizationServiceProxy _serviceProxy;
+    _serviceProxy = new OrganizationServiceProxy(OrganizationUri, HomeRealmUri, Credentials, null);
+
+    _serviceProxy.ServiceConfiguration.CurrentServiceEndpoint.Behaviors.Add(new ProxyTypesBehavior());
+    _ServicioCRM = ((IOrganizationService)_serviceProxy);
+}
